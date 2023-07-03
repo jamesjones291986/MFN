ce3dee18cc8c682688d938d0a3f857c0125b0e8f
jamesjones291986 <59849233+jamesjones291986@users.noreply.github.com>
2023-07-02 22:20:15 -0500

    Updates

--- target_percent.py
+++ target_percent.py
@@ -29,18 +29,17 @@ def get_totals(input_values):
 
 
 play_dict_rf = {'Singeback 4 Wide': ['Singleback 4 Wide Quick Outs'],
-             'Singleback Normal': ['Singleback Normal TE Quick Out'],
+             'Singleback Normal': ['Singleback Normal TE Quick Out', 'Singleback Normal HB Release Mid'],
              'Singleback Slot Strong': ['Singleback Slot Strong HB Checkdown'],
              'Singleback Big': [],
-             'I Formation 3 WR': ['I Formation 3WR WR Out', 'I Formation 3WR Backfield Flats',
-                                  'I Formation 3WR FL Post', 'I Formation 3WR Slot Short WR Deep'],
+             'I Formation 3 WR': ['I Formation 3WR WR Out', 'I Formation 3WR FL Post'],
              'Split Backs 3 Wide': [],
              'Strong I Normal': [],
-             'Weak I Normal': ['Weak I Normal WR Corner TE Middle', 'Weak I Normal Skinny Posts'],
+             'Weak I Normal': ['Weak I Normal Skinny Posts'],
              'I Formation Twin WR': ['I Formation Twin WR Hard Slants', 'I Formation Twin WR Quick Outs'],
-             'I Formation Normal': ['I Formation Normal FL Hitch', 'I Formation Normal Max Protect'],
+             'I Formation Normal': ['I Formation Normal FL Hitch', 'I Formation Normal PA Fullback Flat'],
              'Strong I Big': ['Strong I Big Backfield Drag'],
-             'I Formation Power': ['I Formation Power Play Action HB Downfield', 'I Formation Power PA Flats'],
+             'I Formation Power': ['I Formation Power Play Action HB Downfield'],
              }
 for formation, plays in play_dict_rf.items():
     print(f'{formation}:')
